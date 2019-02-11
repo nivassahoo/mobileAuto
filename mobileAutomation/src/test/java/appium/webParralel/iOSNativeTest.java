5994f2cdeaa051923e754e778127097cc340d362
nivassahoo <30389541+nivassahoo@users.noreply.github.com>
2019-02-11 16:38:43 +0530

    Update iOSNativeTest.java

--- mobileAutomation/src/test/java/appium/webParralel/iOSNativeTest.java
+++ mobileAutomation/src/test/java/appium/webParralel/iOSNativeTest.java
@@ -3,7 +3,21 @@ package appium.webParralel;
 import org.testng.annotations.Test;
 
 public class iOSNativeTest {
+   WebDriverWait wait;
+	
   @Test
-  public void f() {
+  public void test() throws Exception
+  {
+	  wait = new WebDriverWait(driver,60);
+	  wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//XCUIElementTypeStaticText[@name='Login']"))).click();
+	  wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//XCUIElementTypeSecureTextField"))).sendKeys("dbs123");
+	  wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//XCUIElementTypeButton[@name='LOGIN']"))).click();
+	  Thread.sleep(15000);
+	  wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//XCUIElementTypeNavigationBar[@name='dbid_ios.HomeView']/XCUIElementTypeButton[1]"))).click();
+	  wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//XCUIElementTypeStaticText[@name='Transaksi']//.."))).click();
+	  wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//XCUIElementTypeStaticText[@name='Rekening saya']//.."))).click();
+	  wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//XCUIElementTypeStaticText[@name='Transfer']//.."))).click();
+	  Thread.sleep(15000);
   }
+}
 }
