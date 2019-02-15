454933aac5880f4d1c315d2cc467e115145fa655
nivassahoo <30389541+nivassahoo@users.noreply.github.com>
2019-02-15 13:08:36 +0530

    Update hideKeyboard.java

--- mobileAutomation/src/test/java/appium/webParralel/hideKeyboard.java
+++ mobileAutomation/src/test/java/appium/webParralel/hideKeyboard.java
@@ -1,1 +1,34 @@
 
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.remote.HideKeyboardStrategy;
+
+public class hideKeyboard {
+	
+	
+	
+	public void hideKeyboardAndroid(AndroidDriver<MobileElement> driver)
+	{
+		
+	}
+	
+	
+	public static void hideKeyboardiOS(AppiumDriver<MobileElement> driver,String keyType)
+	{
+		 if(keyType.length()<5)
+		 {
+		((IOSDriver<MobileElement>)driver).hideKeyboard(HideKeyboardStrategy.PRESS_KEY, keyType);
+		 }
+		 else if(keyType.equals("")){
+		 ((IOSDriver<MobileElement>)driver).hideKeyboard(HideKeyboardStrategy.PRESS_KEY, "Next");
+		 ((IOSDriver<MobileElement>)driver).hideKeyboard(HideKeyboardStrategy.PRESS_KEY, "Done");
+		 }
+		 else
+		 {
+		 ((IOSDriver<MobileElement>)driver).hideKeyboard();
+		 }
+		 
+	}
+}
