9141980c056cfdcf5398c253d7b10e09b50f582c
nivassahoo <30389541+nivassahoo@users.noreply.github.com>
2019-02-22 18:20:45 +0530

    Update screenOrientation.java

--- mobileAutomation/src/test/java/appium/webParralel/screenOrientation.java
+++ mobileAutomation/src/test/java/appium/webParralel/screenOrientation.java
@@ -1,1 +1,20 @@
 
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.ScreenOrientation;
+import org.testng.annotations.Test;
+
+public class screenOrientation {
+
+
+
+ @Test
+  public void orientation()
+  {
+	  System.out.println("*--*--*-- Current screen orientation Is : " + driver.getOrientation());
+	  driver.rotate(ScreenOrientation.LANDSCAPE);
+	  System.out.println("*--*--*-- Now screen orientation Is : "+ driver.getOrientation());
+  }
+  
+  
+}
